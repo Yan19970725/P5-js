c282ccec19c61b084dd90c3eb84918851a6333d5
Yan19970725 <91224392+Yan19970725@users.noreply.github.com>
2021-10-06 16:15:42 -0400

    assigment

---
+++ scripts.js
@@ -0,0 +1,68 @@
+function setup(){
+  createCanvas(400,400)
+
+}
+function draw(){
+    background(255)
+    fill("red")
+    triangle(0,0,0,40,40,40)
+    fill("blue")
+    triangle(0,0,40,0,40,40)
+    fill(255)
+    rect(0,40,40,160)
+    fill("yellow")
+    rect(0,200,60,60)
+    fill(255)
+    rect(0,40,40,160)
+    rect(0,260,30,140)
+    rect(40,260,160,40)
+    fill("red")
+    rect(30,260,10,100)
+    fill(220)
+    rect(30,360,170,40)
+    fill("blue")
+    rect()
+    fill("blue")
+    triangle(200,260,200,310,250,310)
+    fill("red")
+    triangle(200,260,250,260,250,310)
+    fill("yellow")
+    rect(350,0,50,50)
+    fill("blue")
+    rect(330,330,70,70)
+    fill(255)
+    rect(40,0,260,100)
+    rect(300,0,50,130)
+    rect(150,100,250,60)
+    rect(40,100,60,100)
+    rect(100,100,50,50)
+    rect(100,150,50,100)
+    rect(60,250,340,10)
+    fill("red")
+    rect(60,200,40,20)
+    fill("yellow")
+    rect(120,300,80,60)
+    fill(255)
+    rect(200,310,130,90)
+    rect(350,50,20,50)
+    rect(370,50,30,50)
+    fill("skyblue")
+    rect(200,160,200,90)
+    fill(255)
+    rect(330,250,70,80)
+
+    
+
+
+}
+    
+
+    
+//     if (x > 350){
+//         speed = -1
+// }else if( x < 1){
+//     speed = 1
+// }
+
+// x = x + speed
+// }
